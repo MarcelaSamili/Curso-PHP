519a2f31b661c968fabf11bf7f5144ad3c9eb876
Marcela Samili <85258527+MarcelaSamili@users.noreply.github.com>
2021-08-13 10:09:38 -0300

    Update funçoesaritimeticas.php

--- EasyPHP-Devserver-17/eds-www/aula005/funçoesaritimeticas.php
+++ EasyPHP-Devserver-17/eds-www/aula005/funçoesaritimeticas.php
@@ -25,7 +25,7 @@
 	      $v2 = $_GET["y"];
 	      echo "<h2>Valores recebidos: $v1 e $v2</h2>";
         echo "</br>O valor absoluto de $v2 é ". abs($v2); 
-        echo "</br>O valor de $v1<sup> $v2</sup> é ".pow($v1, $v2); // <sup> e </sup> é pra um número fucar em cima do outro igual quando a gente escreve.
+        echo "</br>O valor de $v1<sup> $v2</sup> é ".pow($v1, $v2); // <sup> e </sup> é pra um número ficar em cima do outro igual quando a gente escreve.
         echo "</br>A raiz de $v1 é ". sqrt($v1);
         echo "</br>O valor de $v2 arrdondado é ". round($v2); //voce pode usar também o "ceil" que sempre arrendonda para cima e o "floor" que sempre arredonda para baixo.
         echo "</br>A parte inteira de $v2 é ". intval($v2);
